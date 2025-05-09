7aee64b3e2ceb6193061961f719a37c9a5bbe230
Aninhaskm <aninhask.42@gmail.com>
2025-05-09 13:08:19 -0400

    correcoes-pylint

--- service/routes.py
+++ service/routes.py
@@ -40,7 +40,10 @@ def list_counters():
     """Lists all counters"""
     app.logger.info("Request to list all counters...")
 
-    counters = [dict(name=count[0], counter=count[1]) for count in COUNTER.items()]
+    counters = [
+        dict(name=count[0], counter=count[1])
+        for count in COUNTER.items()
+    ]
 
     return jsonify(counters)
 
@@ -54,7 +57,8 @@ def create_counters(name):
     app.logger.info("Request to Create counter: %s...", name)
 
     if name in COUNTER:
-        return abort(status.HTTP_409_CONFLICT, f"Counter {name} already exists")
+        msg = f"Counter {name} already exists"
+        return abort(status.HTTP_409_CONFLICT, msg)
 
     COUNTER[name] = 0
 
@@ -91,8 +95,8 @@ def update_counters(name):
     app.logger.info("Request to Update counter: %s...", name)
 
     if name not in COUNTER:
-        msng = f"Counter {name} does not exist"
-        return abort(status.HTTP_404_NOT_FOUND, msng)
+        msg = f"Counter {name} does not exist"
+        return abort(status.HTTP_404_NOT_FOUND, msg)
 
     COUNTER[name] += 1
 
